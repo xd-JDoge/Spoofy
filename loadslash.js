8201a30a8a3ad3b2467ed9e3978560f4ce4ccff9
xd-JDoge <97324764+xd-JDoge@users.noreply.github.com>
2022-01-31 16:48:39 -0500

    Update loadslash.js

--- loadslash.js
+++ loadslash.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-const TOKEN = "ODk3OTI1NTUwNTk5OTcwODY4.YWcwVQ.bIsPEKkf3upA-B8tOpf3awdbbKg"
+require("dotenv").config()
 const client = new Discord.Client({
     intents:  [
         "GUILDS",
@@ -40,4 +40,4 @@ client.on("ready", async () => {
 
 module.exports = bot
 
-client.login(TOKEN)
+client.login(process.env.TOKEN)
